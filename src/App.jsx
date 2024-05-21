410fe4042a4f248a9c542ef2dfa626e584fec6a4
romeo <romeocoliba@gmail.com>
2024-05-21 12:04:05 +0300

    props and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,12 +23,40 @@ function Header() {
   )
 }
 
+function CoreConcept(props) {
+  return(
+    <li>
+      <img src={props.image} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
+function CoreConcepts() {
+  let coreConcepts = [];
+  for (let index = 0; index < CORE_CONCEPTS.length; index++) {
+    coreConcepts.push(
+      <CoreConcept
+        title={CORE_CONCEPTS[index].title}
+        description={CORE_CONCEPTS[index].description}
+        image={CORE_CONCEPTS[index].image}
+      />
+    );
+  }
+
+  return <ul>{coreConcepts}</ul>;
+}
+
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <CoreConcepts />
+        </section>
       </main>
     </div>
   );
