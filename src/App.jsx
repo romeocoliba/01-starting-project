66527b0f1930275ba22738cacf47a2e217ddb729
romeo <romeocoliba@gmail.com>
2024-05-27 10:15:20 +0300

    Reacting to Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Header from './components/Header.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
@@ -10,6 +11,15 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <CoreConcepts />
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+          <TabButton>Component</TabButton>
+          <TabButton>JSX</TabButton>
+          <TabButton>Props</TabButton>
+          <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton(props) {
+
+    function handleClick() {
+        console.log("Clicked");
+    }
+
+    return <li><button onClick={handleClick}>{props.children}</button></li>
+}
